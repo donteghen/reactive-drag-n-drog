4429842d92523d9f2838d6520e2166244a36e8c1
donteghen <donaldteghen@gmail.com>
2024-09-12 07:36:05 +0100

    Complete the implementation and UI styling.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const {
-    fromEvent, takeUntil, concatMap, tap, switchMap, distinctUntilChanged,
+    fromEvent, takeUntil, concatMap, tap, switchMap, distinctUntilChanged, takeLast, first,
     merge
 } = rxjs;
 
-const leftContainer = document.querySelector('#container-left');
-const rightContainer = document.querySelector('#container-right');
+const containerTodoId = 'container-todo';
+const containerProgressId = 'container-progress';
+const containerDoneId = 'container-done';
+
+const todoContainer = document.querySelector(`#${containerTodoId}`);
+const progressContainer = document.querySelector(`#${containerProgressId}`);
+const doneContainer = document.querySelector(`#${containerDoneId}`);
+
+const BOUND_EDGE_TOLERANCE = 20;
+
 
 const wigets = Array.from(document.querySelectorAll('.widget'));
 
@@ -12,25 +20,92 @@ const onWidgetDragStart$ = wigets.map(widget => fromEvent(widget, 'dragstart'));
 const onWidgetDrag$ = wigets.map(widget => fromEvent(widget, 'drag'));
 const onWidgetDragEnd$ = wigets.map(widget => fromEvent(widget, 'dragend'));
 
+const updatePipelineCount = () => {
+    document.querySelector('#todo-count').textContent = todoContainer.children.length;
+    document.querySelector('#progress-count').textContent = progressContainer.children.length;
+    document.querySelector('#done-count').textContent = doneContainer.children.length;
+}
+updatePipelineCount(); // initial page load.
 
-// Subscription to handle the drag-and-drop logic
-const widgetDragging$ = merge(...onWidgetDragStart$).pipe(
-  tap(event => {
-    console.log('Drag started:', event);
-    // Any initial setup, like showing a visual cue, goes here
-  }),
+const removeChild = (parent, child) => {
+    parent.removeChild(child);
+}
+
+const appendChild = (parent, child) => {
+    parent.appendChild(child);
+}
+
+const getNumberFromId = (id) => id.replace(/\D/g, '');
+
+const sortContainer = (container) =>  {  
+    Array.from(container.children)
+    .sort((a, b) => {
+        return getNumberFromId(a.id) - getNumberFromId(b.id)
+    })
+    .forEach(child => {
+        container.appendChild(child)
+    });
+}
+
+const getLinkedContainer = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const currentContainer = document.querySelector(`#${event.target.id}`)?.parentNode;
+    let targetContainer = null;
+    
+    [todoContainer, progressContainer, doneContainer].forEach(container => {
+        const rect = container.getBoundingClientRect();
+
+        const withinX = mouseX >= rect.left - BOUND_EDGE_TOLERANCE && mouseX <= rect.right + BOUND_EDGE_TOLERANCE;
+        const withinY = mouseY >= rect.top - BOUND_EDGE_TOLERANCE && mouseY <= rect.bottom + BOUND_EDGE_TOLERANCE;
+
+        if (withinX && withinY) {
+            targetContainer = container;
+        }
+    });
+    return {targetParent: targetContainer, currentParent: currentContainer};
+}
+
+const handleDragSubscription = (event) => {
+    
+    const {targetParent, currentParent}  = getLinkedContainer(event);
+    const child = document.querySelector(`#${event.target.id}`)
+    
+    if (targetParent && currentParent) { 
+        removeChild(currentParent, child);
+        sortContainer(currentParent);  
+
+        appendChild(targetParent, child);
+        sortContainer(targetParent);  
+        
+        updatePipelineCount()
+    }    
+}
+
+
+
+const widgetDragging$ = merge(...onWidgetDragStart$).pipe(  
   switchMap(() => 
     merge(...onWidgetDrag$).pipe(
         distinctUntilChanged((prev, cur) => {
             return (prev.clientX === cur.clientX && prev.clientY === cur.clientY)
-        }),
-      tap(event => {
-        console.log('Dragging at:', event.clientX, event.clientY);
-        // Handle dragging logic, e.g., update element position
-      }),
-      takeUntil(merge(...onWidgetDragEnd$).pipe(
-        tap(event => console.log('Drag ended:', event))
-      ))
+        }),      
+      takeUntil(merge(...onWidgetDragEnd$)),
+      takeLast(2),
+      first()
     )
   )
-).subscribe();
+).subscribe({
+    next: event => {
+        handleDragSubscription(event)
+    },
+    error: e => {
+        window.alert(e?.message??'OOps something when wrong. Check the logs!');
+        console.log("error:", e);
+    },
+    complete: () => {
+        console.log('Drag done for this element!')
+    }
+});
+
